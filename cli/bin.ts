7a08908e61b2920e717177e4ae110a8392ef4f07
Alberto Benatti <albi.benni8@gmail.com>
2023-08-21 21:55:59 +0200

    basic filtering and folder ignore

--- cli/bin.ts
+++ cli/bin.ts
@@ -2,8 +2,6 @@ import fg from "fast-glob";
 import path from "path";
 import fs from "fs";
 
-console.log("hello, cool");
-
 const dbPath = path.join(
   __dirname,
   "../../../Documents/MyNotes",
@@ -11,3 +9,12 @@ const dbPath = path.join(
 );
 
 const files = fg.sync(path.join(dbPath, "**/*.md"));
+
+const foldersToIgnore = ["Attachement", "node_modules"];
+
+const folders = fs.readdirSync(dbPath).filter((f) => {
+  // Filter out not needed files (.DS_Store, .git,..., etc)
+  return !f.includes(".") && !foldersToIgnore.includes(f);
+});
+
+console.log(folders);
