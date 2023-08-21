9a909e8a6f8384ffb67d438c1889f2f2101f1c2e
Alberto Benatti <albi.benni8@gmail.com>
2023-08-21 17:47:11 +0200

    nav looking cool

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Menu } from "./Menu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50`}>
+        <nav className="p-6 py-4 bg-gray-100 border-t-4 border-t-sky-500 flex items-center space-x-6">
+          <h1 className="font-semibold tracking-tighter text-gray-700">
+            Content Platform
+          </h1>
+          <Menu />
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
