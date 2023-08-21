38e68c4c4f99b561f2e4fefdfcef196ec59c8430
Alberto Benatti <albi.benni8@gmail.com>
2023-08-21 21:49:50 +0200

    got files from vault

--- cli/bin.ts
+++ cli/bin.ts
@@ -1,1 +1,13 @@
+import fg from "fast-glob";
+import path from "path";
+import fs from "fs";
+
 console.log("hello, cool");
+
+const dbPath = path.join(
+  __dirname,
+  "../../../Documents/MyNotes",
+  "ObsidianVault"
+);
+
+const files = fg.sync(path.join(dbPath, "**/*.md"));
