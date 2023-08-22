1349ffd82b8044f0681cfc7b08dccf6c222a4b4e
Alberto Benatti <albi.benni8@gmail.com>
2023-08-22 16:16:43 +0200

    remove todo

--- cli/bin.ts
+++ cli/bin.ts
@@ -5,7 +5,7 @@ import { getFolders } from "./sdk";
 const main = async () => {
   const { folders } = await getFolders();
   const files = await fg(
-    folders.map((folder) => `${folder.absolutePath}/**/*.md`) // TODO: add more folders support
+    folders.map((folder) => `${folder.absolutePath}/**/*.md`)
   );
   // const fileContents = files.map((file) => fs.readFileSync(file, "utf8"));
 
