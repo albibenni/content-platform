13d41737ac9ad58e3f6d71d51bd2a2363a0f155e
Alberto Benatti <albi.benni8@gmail.com>
2023-08-22 16:16:07 +0200

    working md from bin

--- cli/bin.ts
+++ cli/bin.ts
@@ -1,4 +1,19 @@
 import fg from "fast-glob";
 import fs from "fs";
+import { getFolders } from "./sdk";
 
-const files = fg.sync(path.join(dbPath, "**/*.md"));
+const main = async () => {
+  const { folders } = await getFolders();
+  const files = await fg(
+    folders.map((folder) => `${folder.absolutePath}/**/*.md`) // TODO: add more folders support
+  );
+  // const fileContents = files.map((file) => fs.readFileSync(file, "utf8"));
+
+  // console.log(fileContents);
+  console.log(files);
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
